79dc9f899db3f57a9b509e65308c4e7abf44bb11
Simon Wiehe <simon.wiehe@sixrq.com>
2016-08-25 23:40:36 +0100

    KAXB-6 : Add type information

--- src/main/kotlin/com/sixrq/kaxb/AnyElement.kt
+++ src/main/kotlin/com/sixrq/kaxb/AnyElement.kt
@@ -3,6 +3,7 @@ package com.sixrq.kaxb
 class AnyElement(xmlns: String,  primitiveTypeMapping: MutableMap<String, String>) : Element(xmlns, primitiveTypeMapping) {
     init {
         name = "Any"
+        type = "Any"
         imports.add("javax.xml.bind.annotation.XmlAnyElement")
     }
 
