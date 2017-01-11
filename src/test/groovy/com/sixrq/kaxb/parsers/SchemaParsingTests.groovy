b3a5e3d111108e8ad5b553606718d28e9a5907be
Simon Wiehe <simon.wiehe@sixrq.com>
2017-01-11 22:27:07 +0000

    KAXB-8 : Add extra unit tests

--- src/test/groovy/com/sixrq/kaxb/parsers/SchemaParsingTests.groovy
+++ src/test/groovy/com/sixrq/kaxb/parsers/SchemaParsingTests.groovy
@@ -5,13 +5,13 @@ import spock.lang.Specification
 class SchemaParsingTests extends Specification {
     def "A Complex Type with a String field correctly generates a class"() {
         given: "A schema file with a single complex type containing a token type"
-        def parser = new XmlParser("ComplexType.xsd", "com.example")
+        def parser = new XmlParser("StandAloneComplexType.xsd", "com.example")
 
         when: "the classes are generated"
         def classes = parser.generate()
 
         then: "the class is correctly generated"
-        classes.get("ComplexType").toString() == expectedComplexType
+        classes.get("StandAloneComplexType").toString() == expectedStandAloneComplexType
     }
 
     def "A Complex Type with Simple Type elements correctly generates a class"() {
@@ -36,6 +36,78 @@ class SchemaParsingTests extends Specification {
         classes.get("ComplexType").toString() == expectedSimpleTypeWithMaxOccurs
     }
 
+    def "A Complex Type with an include and element of included type correctly generates a class"() {
+        given: "A schema file with a single complex type containing a simple type"
+        def parser = new XmlParser("ComplexTypeWithInclude.xsd", "com.example")
+
+        when: "the classes are generated"
+        def classes = parser.generate()
+
+        then: "the class is correctly generated"
+        classes.get("StandAloneComplexType").toString() == expectedStandAloneComplexType
+        classes.get("IncludeComplexType").toString() == expectedIncludeComplexType
+    }
+
+    def "A Simple Enumerated Type correctly generates an enum"() {
+        given: "A schema file with an enumerated simple type"
+        def parser = new XmlParser("EnumeratedType.xsd", "com.example")
+
+        when: "the classes are generated"
+        def classes = parser.generate()
+
+        then: "the class is correctly generated"
+        classes.get("Enumeration").toString() == expectedEnumType
+    }
+
+
+    def expectedIncludeComplexType = "com.example\n" +
+            "\n" +
+            "import javax.xml.bind.annotation.XmlAccessType\n" +
+            "import javax.xml.bind.annotation.XmlAccessorType\n" +
+            "import javax.xml.bind.annotation.XmlType\n" +
+            "import javax.xml.bind.annotation.XmlElement\n" +
+            "import javax.xml.bind.annotation.XmlSchemaType\n" +
+            "\n" +
+            "/**\n" +
+            "* \n" +
+            "*    A sample complex type for testing\n" +
+            "*    \n" +
+            "*/\n" +
+            "\n" +
+            "\n" +
+            "@XmlAccessorType(XmlAccessType.FIELD)\n" +
+            "@XmlType(name = \"IncludeComplexType\", namespace = \"http://www.garmin.com/xmlschemas/GpxExtensions/v3\", propOrder = arrayOf(\n" +
+            "    \"stringToken\",\n" +
+            "    \"standAloneComplexType\"\n" +
+            "))\n" +
+            "data class IncludeComplexType {\n" +
+            "    @XmlElement(name = \"StringToken\", namespace = \"http://www.garmin.com/xmlschemas/GpxExtensions/v3\")\n" +
+            "    @XmlSchemaType(\"token\")\n" +
+            "    lateinit var stringToken : String\n" +
+            "    @XmlElement(name = \"StandAloneComplexType\", namespace = \"http://www.garmin.com/xmlschemas/GpxExtensions/v3\")\n" +
+            "    lateinit var standAloneComplexType : StandAloneComplexType\n" +
+            "}\n"
+
+    def expectedEnumType = "com.example\n" +
+            "\n" +
+            "import javax.xml.bind.annotation.XmlEnum\n" +
+            "import javax.xml.bind.annotation.XmlEnumValue\n" +
+            "import javax.xml.bind.annotation.XmlType\n" +
+            "\n" +
+            "@XmlType(name = \"Enumeration\", namespace = \"http://www.garmin.com/xmlschemas/GpxExtensions/v3\")\n" +
+            "@XmlEnum\n" +
+            "enum class Enumeration(val value : String ) {\n" +
+            "    @XmlEnumValue(\"Enum1\")\n" +
+            "    ENUM1(\"Enum1\"),\n" +
+            "    @XmlEnumValue(\"Enum2\")\n" +
+            "    ENUM2(\"Enum2\"),\n" +
+            "    @XmlEnumValue(\"Enum3\")\n" +
+            "    ENUM3(\"Enum3\");\n" +
+            "\n" +
+            "    companion object {\n" +
+            "        fun fromValue(value : String ): Enumeration = Enumeration.values().first { it.value == value }\n" +
+            "    }\n" +
+            "}\n"
 
     def expectedSimpleTypeWithMaxOccurs = "com.example\n" +
             "\n" +
@@ -69,7 +141,7 @@ class SchemaParsingTests extends Specification {
             "    var simpleDouble : Double\n" +
             "}\n"
 
-    def expectedComplexType = "com.example\n" +
+    def expectedStandAloneComplexType = "com.example\n" +
             "\n" +
             "import javax.xml.bind.annotation.XmlAccessType\n" +
             "import javax.xml.bind.annotation.XmlAccessorType\n" +
@@ -85,10 +157,10 @@ class SchemaParsingTests extends Specification {
             "\n" +
             "\n" +
             "@XmlAccessorType(XmlAccessType.FIELD)\n" +
-            "@XmlType(name = \"ComplexType\", namespace = \"http://www.garmin.com/xmlschemas/GpxExtensions/v3\", propOrder = arrayOf(\n" +
+            "@XmlType(name = \"StandAloneComplexType\", namespace = \"http://www.garmin.com/xmlschemas/GpxExtensions/v3\", propOrder = arrayOf(\n" +
             "    \"stringToken\"\n" +
             "))\n" +
-            "data class ComplexType {\n" +
+            "data class StandAloneComplexType {\n" +
             "    @XmlElement(name = \"StringToken\", namespace = \"http://www.garmin.com/xmlschemas/GpxExtensions/v3\")\n" +
             "    @XmlSchemaType(\"token\")\n" +
             "    lateinit var stringToken : String\n" +
